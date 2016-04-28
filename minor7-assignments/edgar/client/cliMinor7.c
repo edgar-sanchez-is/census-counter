8086247817423ed68c07923905e2d0dd16eb54d4
Edgar Sanchez <edgr.sanchez@outlook.com>
2016-04-28 18:14:49 -0500

    Adds directory for minor7 assignments

---
+++ minor7-assignments/edgar/client/cliMinor7.c
@@ -0,0 +1,132 @@
+// =====================================================================
+// Name:            Edgar Sanchez
+// Course:          CSCE 3600
+// Date:            April 11, 2016
+// Title:           Minor Assignment 7 Client
+// Version:         1.0
+// Description:     This program will consist of a “main” ticket outlet
+//                  (i.e., the server) that will provide services to
+//                  “BUY” and “RETURN” tickets to two “local” ticket
+//                  distributors (i.e., the clients).
+// Format:          client
+// =====================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+void error(const char *msg)
+{
+	perror(msg);
+	exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+	int sockfd, portno;
+	ssize_t n;
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
+
+	char buffer[256];
+	if (argc < 3) {
+		fprintf(stderr,"usage %s hostname port\n", argv[0]);
+		exit(0);
+	}
+	portno = atoi(argv[2]);
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0)
+		error("ERROR opening socket");
+	server = gethostbyname(argv[1]);
+	if (server == NULL) {
+		fprintf(stderr,"ERROR, no such host\n");
+		exit(0);
+	}
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy(server->h_addr, (char *)&serv_addr.sin_addr.s_addr, (size_t) server->h_length);
+	serv_addr.sin_port = htons(portno);
+	if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+		error("ERROR connecting");
+	}
+
+	char* commands[] = {"BUY", "BUY", "BUY", "BUY", "BUY", "BUY", "BUY", "BUY", "BUY", "BUY", "BUY", "BUY", "BUY", "RETURN ", "BUY"};
+	char ticketDB[15][256];
+	for(int i = 0; i <= 14; ++i) {
+		strncpy(ticketDB[i], "0\0", 2);
+	}
+
+	size_t cmdCount = 0;
+	while (cmdCount <= 14) {
+		bzero(buffer, 256);
+
+		if (strncmp(commands[cmdCount], "BUY", 3) == 0) {
+			printf("[CLIENT] : BUY\n");
+
+			n = write(sockfd, commands[cmdCount], strlen(commands[cmdCount]));
+			if (n < 0) {
+				error("ERROR writing to socket");
+			}
+
+			n = read(sockfd, buffer, 6);
+			if (n < 0) {
+				error("ERROR reading from socket");
+			}
+
+			if (strncmp(buffer, "FULL", 4) == 0) {
+				printf("[SERVER] : Sold out\n");
+				printf("[CLIENT] : Buy failed\n");
+			}
+			else {
+				for(int i = 0; i <= 14; ++i) {
+					if (strcmp(ticketDB[i], "0") == 0) {
+						strncpy(ticketDB[i], buffer, 6);
+						printf("[SERVER] : %s\n", ticketDB[i]);
+						break;
+					}
+				}
+			}
+		}
+		else if (strncmp(commands[cmdCount], "RETURN", 6) == 0) {
+			printf("[CLIENT] : RETURN %s\n", ticketDB[0]);
+
+			sprintf(buffer, "%s%s", commands[cmdCount], ticketDB[0]);
+			n = write(sockfd, buffer, strlen(buffer));
+			if (n < 0) {
+				error("ERROR writing to socket");
+			}
+
+			bzero(buffer, 256);
+			n = read(sockfd, buffer, 8);
+			if (n < 0) {
+				error("ERROR reading from socket");
+			}
+
+			if (strncmp(buffer, "INVALID", 8) == 0) {
+				printf("[SERVER] : Ticket number is invalid\n");
+			}
+			else if (strncmp(buffer, "UNAVAIL", 8) == 0) {
+				printf("[SERVER] : Ticket belongs to different client\n");
+			}
+			else {
+				printf("[SERVER] : RETURN %s\n", ticketDB[0]);
+				strncpy(ticketDB[0], "0\0", 2);
+				printf("[CLIENT] : TICKET %s returned\n", buffer);
+			}
+		}
+		cmdCount++;
+	}
+	close(sockfd);
+
+	printf("[CLIENT] : Ticket Database\n");
+	for(int i = 0; i <= 14; ++i) {
+		printf("%11s[ %2i ] - %s\n", "", i, ticketDB[i]);
+	}
+
+	return EXIT_SUCCESS;
+}
---
+++ minor7-assignments/edgar/server/svrMinor7.c
@@ -0,0 +1,215 @@
+// =====================================================================
+// Name:            Edgar Sanchez
+// Course:          CSCE 3600
+// Date:            April 11, 2016
+// Title:           Minor Assignment 7 Server
+// Version:         1.0
+// Description:     This program will consist of a “main” ticket outlet
+//                  (i.e., the server) that will provide services to
+//                  “BUY” and “RETURN” tickets to two “local” ticket
+//                  distributors (i.e., the clients).
+// Format:          ./server port
+// =====================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <time.h>
+#include <sys/mman.h>
+#include <stdbool.h>
+
+struct Tickets {
+	bool status;
+	int number;
+	size_t client;
+};
+
+// Function prototypes
+void processRequest(int, struct Tickets *ticket, size_t);
+int error(const char *);
+
+int main(int argc, char *argv[]) {
+	// Checks if a port argument was provided
+	if (argc < 2) {
+		fprintf(stderr,"ERROR, no port provided\n");
+		return EXIT_FAILURE;
+	}
+
+	// Declare variables
+	int sockfd, newsockfd, portno;
+	pid_t pid;
+	socklen_t clilen;
+	struct sockaddr_in serv_addr, cli_addr;
+
+	// Opens INET TCP socket
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) {
+		error("ERROR opening socket");
+	}
+
+	// Initializes socket structure
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	portno = atoi(argv[1]);
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portno);
+
+	// Binds the host address using bind() function
+	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+		error("ERROR on binding");
+	}
+
+	// Listens for clients and waits for incoming connections
+	listen(sockfd,5);
+	clilen = sizeof(cli_addr);
+
+	// Creates base ticket struct then creates a shared memory ticket struct pointing to base
+	struct Tickets ticketBase[20];
+	struct Tickets *ticket = mmap(ticketBase, sizeof(struct Tickets), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);;
+
+	// Generates random ticket numbers and initializes ticket properties
+	srand((unsigned int) time(NULL));
+	for(int i = 0; i <= 19; ++i) {
+		ticket[i].status = true;
+		ticket[i].number = rand() % 90000 + 10000;
+		ticket[i].client = 0;
+	}
+
+	// Client connection loop (allows two connections)
+	size_t clientNum = 1;
+	while (clientNum <= 2) {
+		// Waits until client connects then stores new file descriptor in newsockfd
+		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+		if (newsockfd < 0) {
+			error("ERROR on accept");
+		}
+
+		// Forks and processes client requests in child
+		pid = fork();
+		if (pid < 0) {
+			// Error
+			error("ERROR on fork");
+		}
+		else if (pid == 0) {
+			// Child Process
+			close(sockfd);
+			processRequest(newsockfd, ticket, clientNum);
+			return EXIT_SUCCESS;
+		}
+		else {
+			// Parent Process
+			close(newsockfd);
+			clientNum++;
+		}
+	}
+
+	// Waits for forked processes to terminate and gracefully exits
+	wait(NULL);
+	wait(NULL);
+	close(sockfd);
+	munmap(ticket, sizeof(struct Tickets));
+	return EXIT_SUCCESS;
+}
+
+// Processes ticketing requests from clients
+void processRequest(int sock, struct Tickets *ticket, size_t clientNum)
+{
+	while (1) {
+		ssize_t n;
+		char buffer[256];
+		bzero(buffer,256);
+
+		// Reads from client and stores in buffer
+		n = read(sock,buffer,255);
+		if (n == 0) {
+			break;
+		}
+		else if (n < 0) {
+			error("ERROR reading from socket");
+		}
+
+		// Splits client input into command and ticket number if provided
+		char* command = strtok(buffer, " ");
+		if (command == NULL) {
+			error("ERROR splitting command");
+		}
+		char* returnNum = strtok(NULL, " ");
+
+		// Processes commands and modifies tickets as requested
+		if (strncmp(command, "BUY", 3) == 0) {
+			printf("[CLIENT %zu] : %s\n", clientNum, command);
+			for(int i = 0; i <= 19; ++i) {
+				if (ticket[i].status == true) {
+					printf("[SERVER X] : Client %zu bought %i\n", clientNum, ticket[i].number);
+					ticket[i].status = false;
+					ticket[i].client = clientNum;
+
+					char text[6];
+					sprintf(text, "%d", ticket[i].number);
+					n = write(sock, text, 6);
+					if (n < 0) {
+						error("ERROR writing to socket");
+					}
+					break;
+				}
+				else if (ticket[19].status == false && i == 19) {
+					printf("[SERVER X] : Sold out\n");
+					n = write(sock, "FULL", 5);
+					if (n < 0) {
+						error("ERROR writing to socket");
+					}
+					break;
+				}
+			}
+		}
+		else if (strncmp(command, "RETURN", 6) == 0) {
+			printf("[CLIENT %zu] : %s %s\n", clientNum, command, returnNum);
+			for(int i = 0; i <= 19; ++i) {
+				if (ticket[i].number == atoi(returnNum) && ticket[i].client == clientNum && ticket[i].status == false) {
+					printf("[SERVER X] : Client %zu returned %i\n", clientNum, ticket[i].number);
+					ticket[i].status = true;
+					ticket[i].client = 0;
+
+					char text[8];
+					sprintf(text, "%i", ticket[i].number);
+					n = write(sock, text, 8);
+					if (n < 0) {
+						error("ERROR writing to socket");
+					}
+					break;
+				}
+				else if (ticket[i].number == atoi(returnNum) && ticket[i].client != clientNum && ticket[i].status == false) {
+					printf("[SERVER X] : Client %zu does not own %i\n", clientNum, ticket[i].number);
+					n = write(sock, "UNAVAIL", 8);
+					if (n < 0) {
+						error("ERROR writing to socket");
+					}
+				}
+				else if (i == 19) {
+					printf("[SERVER X] : Invalid ticket number %s\n", returnNum);
+					n = write(sock, "INVALID", 8);
+					if (n < 0) {
+						error("ERROR writing to socket");
+					}
+				}
+			}
+		}
+		else {
+			fprintf(stderr, "[CLIENT %zu] : Unknown command: %s", clientNum, command);
+			n = write(sock, "Unknown command\n", 16);
+			if (n < 0) {
+				error("ERROR writing to socket");
+			}
+		}
+	}
+}
+
+int error(const char *msg) {
+	perror(msg);
+	return EXIT_FAILURE;
+}
