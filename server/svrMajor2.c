5b0b50be241fcc9a5f7f80b2ecdde157e03125d5
Edgar Sanchez <edgr.sanchez@outlook.com>
2016-04-30 05:11:57 -0500

    Updated branch from master

---
+++ server/svrMajor2.c
@@ -0,0 +1,18 @@
+// =====================================================================
+// Names:           Alex, Edgar, Luis, Russell
+// Course:          CSCE 3600
+// Due Date:        May 4, 2016
+// Title:           Major Assignment 2 - Server
+// Version:         1.0
+// Description:     This program will consist of a client/server and
+//                  client/client model using Linux sockets for a
+//                  "census" system using the UNT Apollo server.
+// Format:          ./server <port>
+// =====================================================================
+
+#include <stdio.h>
+
+int main() {
+	printf("Hello world!\n");
+	return EXIT_SUCCESS;
+}
