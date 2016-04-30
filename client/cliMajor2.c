e92ffbec88cdd31b35d29e27052a2783411c027e
Edgar Sanchez <edgr.sanchez@outlook.com>
2016-04-30 05:08:54 -0500

    Added Makefiles and rearranged structure. Tested Apollo, files, etc.

---
+++ client/cliMajor2.c
@@ -0,0 +1,19 @@
+// =====================================================================
+// Names:           Alex, Edgar, Luis, Russell
+// Course:          CSCE 3600
+// Due Date:        May 4, 2016
+// Title:           Major Assignment 2 - Client
+// Version:         1.0
+// Description:     This program will consist of a client/server and
+//                  client/client model using Linux sockets for a
+//                  "census" system using the UNT Apollo server.
+// Format:          ./client1 <rem_ipaddr> <svr_port> <cli1_port> <cli2_port> <cpu_%>
+// =====================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	printf("Hello world! I am a client!\n");
+	return EXIT_SUCCESS;
+}
---
+++ server/svrMajor2.c
@@ -0,0 +1,19 @@
+// =====================================================================
+// Names:           Alex, Edgar, Luis, Russell
+// Course:          CSCE 3600
+// Due Date:        May 4, 2016
+// Title:           Major Assignment 2 - Server
+// Version:         1.0
+// Description:     This program will consist of a client/server and
+//                  client/client model using Linux sockets for a
+//                  "census" system using the UNT Apollo server.
+// Format:          ./server <port>
+// =====================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	printf("Hello world! I am a server!\n");
+	return EXIT_SUCCESS;
+}
